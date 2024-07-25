57d5d8ab653a51a1edd2b0b6a1fc6b1e6a6e94ec
nitish-vashisth <nitishlvashisth@gmail.com>
2024-07-25 18:52:28 +0530

    767. Reorganize String

--- LeetCode/ReorganizeString.java
+++ LeetCode/ReorganizeString.java
@@ -1,3 +1,5 @@
+package LeetCode;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
--- LeetCode/SubarraySumsDivisiblebyK.java
+++ LeetCode/SubarraySumsDivisiblebyK.java
@@ -1,3 +1,5 @@
+package LeetCode;
+
 public class SubarraySumsDivisiblebyK {
 
     /*
